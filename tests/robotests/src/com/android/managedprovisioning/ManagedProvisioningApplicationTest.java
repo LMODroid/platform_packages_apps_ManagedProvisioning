7fd7d59b17ce95e89e2b053c3590568137143e88
shreyacsingh <shreyacsingh@google.com>
2022-12-14 16:34:19 +0000

    Deprecating provisioning param- EXTRA_PROVISIONING_KEEP_SCREEN_ON, removing relevant code. From now on always keep the screen on throughout the provisioning flow.
    Bug: 259249103
    Fix: 259249103
    Test: not required
    Change-Id: Icfc97f22c0c99004c051860021d995cb3cd951a4

--- tests/robotests/src/com/android/managedprovisioning/ManagedProvisioningApplicationTest.java
+++ tests/robotests/src/com/android/managedprovisioning/ManagedProvisioningApplicationTest.java
@@ -16,48 +16,16 @@
 
 package com.android.managedprovisioning;
 
-import static android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
-
-import static com.google.common.truth.Truth.assertThat;
-
-import static org.robolectric.Shadows.shadowOf;
-
 import android.app.Activity;
 
-import org.junit.Ignore;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.shadows.ShadowWindow;
 
 @RunWith(RobolectricTestRunner.class)
 public final class ManagedProvisioningApplicationTest {
     private final ManagedProvisioningApplication mApplication =
             new ManagedProvisioningApplication();
-
-    @Ignore("b/218480743")
-    @Test
-    public void markKeepScreenOn_works() {
-        Activity activity = createActivity();
-        ShadowWindow shadowWindow = shadowOf(activity.getWindow());
-        mApplication.markKeepScreenOn();
-
-        mApplication.maybeKeepScreenOn(activity);
-
-        assertThat(shadowWindow.getFlag(FLAG_KEEP_SCREEN_ON)).isTrue();
-    }
-
-    @Ignore("b/218480743")
-    @Test
-    public void markKeepScreenOn_screenNotMarkedOn_flagNotSet() {
-        Activity activity = createActivity();
-        ShadowWindow shadowWindow = shadowOf(activity.getWindow());
-
-        mApplication.maybeKeepScreenOn(activity);
-
-        assertThat(shadowWindow.getFlag(FLAG_KEEP_SCREEN_ON)).isFalse();
-    }
 
     private Activity createActivity() {
         return Robolectric.buildActivity(Activity.class)
